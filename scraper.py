b5149c2d34b27a04a7da4bdfe87938ae6dae89e0
SerbanSlincu <serbanslincu@gmail.com>
2019-10-15 01:48:51 +0100

    Easier code maintenance

--- scraper.py
+++ scraper.py
@@ -1,38 +1,62 @@
 import requests
+import datetime
 
-# getting the new content
-content = requests.get("https://news.ycombinator.com/").text
-entries = content.split("\n")
-lines = []; maxLength = 0
+# getting the latest content
+def getContent():
+    content = requests.get("https://news.ycombinator.com/").text
+    entries = content.split("\n")
+    lines = []; maxLength = 0
 
-for entry in entries:
-    if("<span class=\"rank\">" not in entry):
-        continue
+    for entry in entries:
+        if("<span class=\"rank\">" not in entry):
+            continue
 
-    rank = entry.split("class=\"rank\"")[1].split(">")[1].split(".")[0]
-    link = entry.split("td class=\"title\"")[1].split(">")[1].split("<a href=\"")[1].split("\"")[0]
-    title = entry.split("class=\"storylink\"")[1].split(">")[1].split("</a")[0]
+        rank = entry.split("class=\"rank\"")[1].split(">")[1].split(".")[0]
+        link = entry.split("td class=\"title\"")[1].split(">")[1].split("<a href=\"")[1].split("\"")[0]
+        title = entry.split("class=\"storylink\"")[1].split(">")[1].split("</a")[0]
 
-    line = rank + title + link
-    lines.insert(0, (rank, title, link))
-    maxLength = max(maxLength, len(line) + 10)
-
-lines.reverse()
+        line = rank + title + link
+        lines.insert(0, (rank, title, link))
+        maxLength = max(maxLength, len(line))
+    
+    lines.reverse()
+    return (lines, maxLength)
 
 # printing the top-most results
-class bcolors:
-    pink = '\033[95m'
-    blue = '\033[94m'
-    green = '\033[92m'
-    orange = '\033[93m'
-    red = '\033[91m'
-    unbold = '\033[0m'
-    bold = '\033[1m'
-    underline = '\033[4m'
-for line in lines:
-    print(bcolors.bold + bcolors.red + line[0] + ". " + bcolors.orange + line[1] + ": " + bcolors.pink + line[2])
+def checkContent(lines, logFilePath):
+    if(len(lines) != 30):
+        f = open(logFilePath, "a")
+        f.write(str(datetime.datetime.utcnow()) + " -- WARNING -- Less than 30 articles read! Only {} scraped.\n".format(len(lines)))
+        f.close()
+        
+# printing the top-most results
+def printContent(lines):
+    class bcolors:
+        pink = '\033[95m'
+        blue = '\033[94m'
+        green = '\033[92m'
+        orange = '\033[93m'
+        red = '\033[91m'
+        unbold = '\033[0m'
+        bold = '\033[1m'
+        underline = '\033[4m'
+    for line in lines:
+        print(bcolors.bold + bcolors.red + line[0] + ". " + bcolors.orange + line[1] + ": " + bcolors.pink + line[2])
 
 # adding to past results
-for line in lines:
-    if(not seenBefore(line)):
-        addCurrent(line)
+def storeContent(lines):
+    for line in lines:
+        if(not seenBefore(line)):
+            addCurrent(line)
+
+# put everything together
+def main():
+    logFilePath = "app.log"
+    (lines, maxLength) = getContent()
+
+    checkContent(lines, logFilePath)
+    printContent(lines)
+    #storeContent(lines, storeFilePath)
+
+if __name__ == "__main__":
+    main()
